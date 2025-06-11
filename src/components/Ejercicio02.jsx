3e658cefaf2592d9ac2e48978455da63edb640d5
morales80 <164915603+morales80@users.noreply.github.com>
2025-06-11 17:03:54 -0600

    [UPDATE:se agrego el boton de añadir articulo y borrar articulo al ej02

--- src/components/Ejercicio02.jsx
+++ src/components/Ejercicio02.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
-
 const Ejercicio02 = () => {
-  return (
-    <div className="row mt-4">
-        <div className="col-12">
-            <h2 className="h4 mt-4">Ejercicio 02 - Boton y funciones</h2>
-            <hr className='bg-primary' />
-            <br />
-            <div className="span d-block text-center bg-primary text-white p-4">
-                este es un componente
+    const nombreProducto = "Camisa";
+
+    function agregarACarritoCompra() {
+        console.log(":::::::Agregando articulo: " + nombreProducto + "::::::::");
+    }
+    function eliminarDeCarritoCompra() {
+        console.log(":::::::Eliminando articulo: " + nombreProducto + "::::::::");
+    }
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <h2 className="h4 mt-4">Ejercicio 02 - Boton y funciones</h2>
+                    <hr className='bg-primary' />
+                </div>
+                <div className="col-12 col-md-6 mb-2 d-flex justify-content-center">
+                    <button className="btn btn-primary btn-outline-warning btn-lg w-100" onClick={agregarACarritoCompra}>
+                        <i className="bi bi-openai"></i>
+                        <span className='ms-3'>
+                            Agregar Articulo
+                        </span>
+                    </button>
+                </div>
+                <div className="col-12 col-md-6 mb-2 d-flex justify-content-center">
+                    <button className='btn btn-primary btn-outline-warning btn-lg w-100' onClick={eliminarDeCarritoCompra}>
+                        <i className="bi bi-trash3"></i>
+                        <span className='ms-3'>Borrar articulo</span>
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    );
 }
 
 export default Ejercicio02
